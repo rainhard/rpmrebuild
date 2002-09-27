e37d667c944d1ba3a572cacffc166b1cb0c48668
gerbier <gerbier@f0fe687d-0691-4e6f-908c-fb32f6a794c4>
2002-09-27 13:23:36 +0000

    big change from Valery Reznic <valery_reznic@users.sourceforge.net>

--- src/src/rpmrebuild.sh
+++ src/src/rpmrebuild.sh
@@ -3,7 +3,7 @@
 #   rpmrebuild.sh 
 #
 #    Copyright (C) 2002 by Eric Gerbier
-#    Bug reports to:  gerbier@users.sourceforge.net
+#    Bug reports to: gerbier@users.sourceforge.net
 #    $Id$
 #
 #    This program is free software; you can redistribute it and/or modify
@@ -18,51 +18,110 @@
 #
 ###############################################################################
 
+function Usage
+{
+	echo -e "\nrpmrebuild.sh is a tool to rebuild an rpm file from the rpm database"
+	echo "it does not need to be run under root user"
+	echo "syntaxe : $0 package"
+	echo "the spec and rpm result are built on local directory"
+	echo "Copyright (C) 2002 by Eric Gerbier"
+	echo -e "this program is distributed under GNU General Public License\n"
+}
+###############################################################################
+# build general tags
+function SpecFile
+{
+   QF="\
+[Name:          %{NAME}\n]\
+[Version:       %{VERSION}\n]\
+[Release:       %{RELEASE}\n]\
+[URL:           %{URL}\n]\
+[Distribution:  %{DISTRIBUTION}\n]\
+[Vendor:        %{VENDOR}\n]\
+[Packager:      %{PACKAGER}\n]\
+[License:       %{LICENSE}\n]\
+[Group:         %{GROUP}\n]\
+[Summary:       %{SUMMARY}\n]\
+[BuildArch:     %{ARCH}\n]\
+[Epoch:         %{EPOCH}\n]\
+[Icon:          %{ICON}\n]\
+[XPM:           %{XPM}\n]\
+[GIF:           %{GIF}\n]\
+[Requires:      %{REQUIRENAME} %{REQUIREFLAGS:depflags} % {REQUIREVERSION}\n]\
+[Conflicts:     %{CONFLICTNAME} %{CONFLICTFLAGS:depflags} % {CONFLICTVERSION}\n]\
+[Obsoletes:     %{OBSOLETES}\n]\
+[Provides:      %{PROVIDES}\n]\
+[ExcludeArch:   %{EXCLUDEARCH}\n]\
+[ExclusiveArch: %{EXCLUSIVEARCH}\n]\
+[ExcludeOs:     %{EXCLUDEOS}\n]\
+[ExclusiveOs:   %{EXCLUSIVEOS}\n]\
+[Prefix:        %{PREFIXES}\n]\
+[\n%%description\n %{DESCRIPTION}\n\n]\
+[%%trigger%{TRIGGERTYPE} -p %{TRIGGERSCRIPTPROG} -- %{TRIGGERCONDS} \n%{TRIGGERSCRIPTS}\n]\
+[%%pre -p %{PREINPROG}\n%{PREIN}\n\n]\
+[%%post -p %{POSTINPROG}\n%{POSTIN}\n\n]\
+[%%preun -p %{PREUNPROG}\n%{PREUN}\n\n]\
+[%%postun -p %{POSTUNPROG}\n%{POSTUN}\n\n]\
+[%%verify -p %{VERIFYSCRIPTPROG}\n%{VERIFYSCRIPT}\n\n]\
+%|CHANGELOGTIME?{%%changelog\n[* %{CHANGELOGTIME:day} %{CHANGELOGNAME} \n\n%{CHANGELOGTEXT}\n\n]\n}:{}|\
+"
+# query of require may duplicate line, so disable automatic
+echo "AutoReq: no"
+echo "AutoProv: no"
+rpm --query --queryformat "${QF}" ${PAQUET}
+
+cat << END
+%prep
+%build
+%install
+%clean
+
+END
+}
+###############################################################################
+# build the list of files in package
+function FilesSpecFile
+{
+echo "%files"
+
+QF="[%{FILENAMES} %{FILEFLAGS:fileflags}\n]"
+rpm --query --queryformat "${QF}" ${PAQUET} | while read file type
+do
+	if [ ! -e $file ]
+	then
+		# skip missing files
+		echo "# $file"
+	elif [ -d $file ]
+	then
+		# special case for directories
+		echo "%dir $file"
+	elif [ "$type" = "d" ]
+	then
+		# test for documentation files
+		echo "%doc $file"
+	elif [ "$type" = "c" ]
+	then
+		# configuration file
+		echo "%config $file"
+	else
+		# default
+		echo "$file"
+	fi
+done
+}
+
+##############################################################
+# Main Part                                                  #
+##############################################################
 # shell pour refabriquer un fichier rpm a partir de la base rpm
 # a shell to build an rpm file from the rpm database
-
-#####################################################
-usage() {
-	echo "syntaxe : $0 package"
-	echo "build an rpm package from rpm database"
-}
-#####################################################
-interrogation() {
-	rpm -q --queryformat "$1" ${PAQUET}
-}
-
-#####################################################
-# write_info rpm_tag rpm_query
-write_info() {
-	rpm_tag=$1
-	rpm_query=$2
-
-	output=$(interrogation "$rpm_query")
-	if [ -z "$output" ]
-	then
-		return
-	elif [ "$output" != "(none)" ]
-	then
-		echo -e "$rpm_tag $output" >> ${FIC_SPEC}
-	fi
-}
-
-#####################################################
 
 # test argument
 if [ $# -ne 1 ]
 then
-	usage
+	Usage
 	exit 1 
 fi
-
-# test root
-#out=$(echo $HOME | grep root)
-#if [ -z "$out" ]
-#then
-#        echo "you have to work under root user"
-#        exit
-#fi
 
 # suite a des probleme de dates incorrectes
 # to solve problems of bad date
@@ -81,13 +140,6 @@ else
 	then
 		echo "WARNING : too much packages match ${PAQUET} : $output"
 		exit 1
-	else
-		NAME=$(interrogation '%{NAME}\n')
-		VERSION=$(interrogation '%{VERSION}\n')
-		RELEASE=$(interrogation '%{RELEASE}\n')
-
-		NOM_COMPLET=${NAME}-${VERSION}-${RELEASE}
-		NOM_VERSION=${NAME}-${VERSION}
 	fi
 fi
 
@@ -110,7 +162,7 @@ fi
 
 # fabrication fichier spec
 # build spec file
-FIC_SPEC=${NOM_COMPLET}.spec
+FIC_SPEC=./${PAQUET}.spec
 
 if [ -a ${FIC_SPEC} ]
 then
@@ -118,148 +170,17 @@ then
 	mv -f ${FIC_SPEC} ${FIC_SPEC}.sav
 fi
 
-write_info "Summary: " '%{SUMMARY}\n'
-write_info "Name: "    '%{NAME}\n'
-write_info "Version: " '%{VERSION}\n'
-write_info "Release: " '%{RELEASE}\n'
-
-# not used Patch and Source
-
-write_info "URL: " '%{URL}\n'
-write_info "Distribution: " '%{DISTRIBUTION}\n'
-write_info "Vendor: " '%{VENDOR}\n'
-write_info "Packager: " '%{PACKAGER}\n'
-write_info "Copyright: " '%{COPYRIGHT}\n'
-write_info "Group: "  '%{GROUP}\n'
-write_info "BuildArch: " '%{ARCH}\n'
-ARCH=$(interrogation '%{ARCH}\n')
-
-# query of require may duplicate line, so disable automatic
-echo "AutoReqProv: no" >> ${FIC_SPEC}
-write_info "Requires: " '[%{REQUIRENAME} %{REQUIREFLAGS:depflags} %{REQUIREVERSION} ]\n'
-write_info "Conflicts: " '[%{CONFLICTNAME} %{CONFLICTFLAGS:depflags} %{CONFLICTVERSION} ]\n'
-write_info "Obsoletes: " '[%{OBSOLETES} ]\n'
-write_info "Provides: " '[%{PROVIDES} ]\n'
-
-#echo "BuildRoot: %{_builddir}/%{name}" >> ${FIC_SPEC}
-
-# some rare tags
-write_info "Serial: " '%{SERIAL}\n'
-write_info "Icon: " '%{ICON}\n'
-write_info "ExcludeArch: " '[%{EXCLUDEARCH} ]\n'
-write_info "ExclusiveArch: " '[%{EXCLUSIVEARCH} ]\n'
-write_info "ExcludeOs: " '[%{EXCLUDEOS} ]\n'
-write_info "ExclusiveOS: " '[%{EXCLUSIVEOS} ]\n'
-
-write_info "%description\n" '%{DESCRIPTION}\n'
-
-cat << END >>  ${FIC_SPEC}
-%prep
-%build
-%install
-%clean
-
-END
-
-write_info "%pre\n" '%{PREINPROG}\n'
-write_info "%preun\n" '%{PREUNPROG}\n'
-write_info "%post\n" '%{POSTINPROG}\n'
-write_info "%postun\n" '%{POSTUNPROG}\n'
-
-# code from Han Holl
-S=/tmp/trigger$$
-EOL="*#*#*#*#"
-rpm -q --qf "[%{TRIGGERTYPE}\n%{TRIGGERSCRIPTPROG}\n%{TRIGGERCONDS}\n%{TRIGGERSCRIPTS}\n$EOL\n]" $PAQUET >$S
-if [ -s $S ]
-then
-	while read ttype
-	do
-          read tshell
-          [ "$tshell" = "/bin/sh" ] && tshell="" || tshell="-p $tshell"
-          read tcond
-          read tline
-          echo "%trigger${ttype} $tshell -- $tcond"
-          while [ "$tline" != $EOL ]
-          do
-            echo $tline
-            read tline
-          done
-	done <$S  >>${FIC_SPEC}
-fi
-rm -f $S
-
-cat << END2 >>  ${FIC_SPEC}
-%files
-END2
-
-listeall=$(rpm -ql ${PAQUET})
-listedoc=$(rpm -qld ${PAQUET})
-listeconfig=$(rpm -qlc ${PAQUET})
-for file in $listeall
-do
-	# petite subtilite : les repertoires ne doivent pas 
-	# apparaitre directement, sinon probleme de doublons
-	if [ -d $file ]
-	then
-		echo "%dir $file" >> ${FIC_SPEC}
-		echo "%dir $file"
-	else
-		# test for doc files
-		for ficdoc in $listedoc
-		do
-			if [ "$file" = "$ficdoc" ]
-			then
-				echo "%doc $file" >> ${FIC_SPEC}
-				echo "%doc $file"
-				continue 2
-			fi
-		done
-		# test for config files
-		for ficconf in $listeconfig
-		do
-			if [ "$file" = "$ficconf" ]
-			then
-				echo "%config $file" >> ${FIC_SPEC}
-				echo "%config $file"
-				continue 2
-			fi
-		done
-		
-		# default
-		echo "$file" >> ${FIC_SPEC}
-		echo "$file"
-	fi
-done
-
-echo "%changelog" >> ${FIC_SPEC}
-rpm -q --changelog ${PAQUET} >> ${FIC_SPEC}
+QF=""
+{
+   SpecFile &&
+   FilesSpecFile
+} > ${FIC_SPEC}
 
 # reconstruction fichier rpm : le src.rpm est inutile
 # build rpm file, the src.rpm is not usefull to do
+rpm -bb --quiet --define "_rpmdir $PWD/" ${FIC_SPEC}
 
-# just an hint to build the rpm file in local dir
-fmacro=~/.rpmmacros
-# save it
-if [ -f $fmacro ]
-then
-        cp -a $fmacro ${fmacro}.sav
-fi
-
-# change rpmdir
-echo "%_rpmdir ." >> $fmacro
-
-rpm -bb ${FIC_SPEC}
-
-# restore it
-if [ -f ${fmacro}.sav ]
-then
-        mv ${fmacro}.sav $fmacro
-fi
-
-
-echo "##################################################################"
-echo "result :"
-ls -l ${ARCH}/${PAQUET}-${VERSION}-${RELEASE}.${ARCH}.rpm
-
-# be carefull, there is others files not cleaned :
-echo "you may clean too : ${FIC_SPEC} "
+QF_RPMFILENAME=$(rpm --eval %_rpmfilename)
+RPMFILENAME=$(rpm --query --queryformat "${QF_RPMFILENAME}" ${PAQUET})
+echo "result: ${RPMFILENAME}"
+exit 0
