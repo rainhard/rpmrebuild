adc8d5283c1970a459f1723f99280581292d1c1a
valery_reznic <valery_reznic@f0fe687d-0691-4e6f-908c-fb32f6a794c4>
2004-09-28 14:12:38 +0000

    Indentation

--- src/src/rpmrebuild_files.sh
+++ src/src/rpmrebuild_files.sh
@@ -36,6 +36,7 @@
 #                            from filesystem, otherwise - from package query
 #   RPMREBUILD_COMMENT_MISSING  - 'yes' - comment missing files,
 #                                  otherwise - nothing
+#   RPMREBUILD_WARNING - yes  -  warn, if filename has glob characters
 ################################################################
 
 FFLAGS="d c s m n g"
@@ -50,126 +51,127 @@ g_val="%ghost "    # ghost
 VERIFY_FLAGS="md5 size link user group mtime mode rdev"
 
 while :; do
-   read file_type
-   [ "x$file_type" = "x" ] && break
-   read file_flags
-   read file_perm
-   read file_user
-   read file_group
-   read file_verify
-   read file_lang
-   read file
+	read file_type
+	[ "x$file_type" = "x" ] && break
+	read file_flags
+	read file_perm
+	read file_user
+	read file_group
+	read file_verify
+	read file_lang
+	read file
 
-   # bash 2 syntaxe
-   #[[ $file = *\** ]] && file=$(echo "$file"|sed 's/\*/\\*/')
-   # bash 1 but with a fork (grep)
-   #wild=$(echo $file | grep "\*")
-   #[ -n "$wild" ] && file=$(echo "$file"|sed 's/\*/\\*/')
-   # quick and portable
-   case "x$file" in
-      x*\**) file=$(echo "$file"|sed 's/\*/\\*/');;
-      x*) ;;
-   esac
-   miss_str=""
-   if [ "X$RPMREBUILD_COMMENT_MISSING" = "Xyes" ]; then
-      if [ -e "$file" ]; then
-         miss_str=""
-      else 
-         miss_str='# MISSING: '
-      fi
-   fi
-
-   # language handling
-   [ "X$file_lang" = "X(none)" ] && file_lang=""
-   lang_str=""
-   [ "X$file_lang" = "X" ] || lang_str="%lang($file_lang) "
-   
-   # %dir handling
-   dir_str=""
-   [ "X$file_type" = "Xd" ] && dir_str="%dir "
-
-   # %fflags handling
-   if [ "X$file_flags" = "X" ]; then
-      fflags_str=""
-   else
-      for flag in $FFLAGS; do
-         if [ "X${file_flags##*${flag}*}" = "X" ]; then
-            eval ${flag}_str="\$${flag}_val"
-         else
-            eval ${flag}_str=""
-         fi
-      done
-
-      # Reset strings' values
-      config_par=""; config_full=""
-
-      config_par="${m_str}${n_str}"
-      # Handle a rpm's bug described by Han Holl:
-      # There are missignok or/and noreplace flag but no config flag
-      # In this case I simple force using '%config' string
-      [ "X$config_par" = "X" ] || c_str=$c_val
-
-      # Conacatenate c_str with config_param. 
-      # If config param non-empty strip it's last character
-      config_full="${c_str}${config_par:+(${config_par%?})}" 
-
-      # If config_full string non-empty add space
-      config_full="${config_full:+${config_full} }"
-
-      fflags_str="${d_str}${config_full}${g_str}"
-   fi
-
-   # %attr handling
-   if [ "X$RPMREBUILD_PUG_FROM_FS" = "Xyes" ]; then
-   	attr_str=""
-   else
-   	file_perm="${file_perm#??}"
-   	attr_str="%attr($file_perm $file_user $file_group) "
-   fi
-
-   # Verify handling
-   verify_str=""
-   verify_par=""
-   non_verify_par=""
-   Bit=1
-   file_verify="0$file_verify" # make it octal for shell
-   for verify_flag in $VERIFY_FLAGS; do
-      if [ $[$file_verify & $Bit] -eq 0 ]; then
-         non_verify_par="$non_verify_par$verify_flag "
-      else
-         verify_par="$verify_par$verify_flag "
-      fi
-      Bit=$[ $Bit << 1 ]
-   done
-
-   # if bit after last verify bit is off I assume that %verify( ...) was used
-   # otherwise I assume %verify(not ...) was used. 
-   if [ $[$file_verify & $Bit] -eq 0 ]; then ## Use "verify_par"
-      # If verify_par not empty, set verify_str to %verify($verify_par)
-      # Strip last character from verify_par
-      verify_str="${verify_par:+%verify(${verify_par%?}) }" 
-   else # Use "non_verify_par
-      # If non_verify_par not empty, set verify_str to %verify(not $verify_par)
-      # Strip last character from non_verify_par
-      verify_str="${non_verify_par:+%verify(not ${non_verify_par%?}) }" 
-   fi
-
-   # test for jokers in file : globing seems not to work
-   # for performance reason, just if warning flag
-   if [ "X$RPMREBUILD_WARNING" = "Xyes" ]; then
-	case "$file" in
-      		*[*?]*)
-			cat <<-WARN_TXT 1>&2 || exit
-			
-			-------------------------------- WARNING ------------------------------------------
-			file named $file contains globbing characters
-			rpm building may not work
-			-----------------------------------------------------------------------------------
-			WARN_TXT
-		;;
+	# bash 2 syntaxe
+	#[[ $file = *\** ]] && file=$(echo "$file"|sed 's/\*/\\*/')
+	# bash 1 but with a fork (grep)
+	#wild=$(echo $file | grep "\*")
+	#[ -n "$wild" ] && file=$(echo "$file"|sed 's/\*/\\*/')
+	# quick and portable
+	case "x$file" in
+		x*\**) file=$(echo "$file" | sed 's/\*/\\*/');;
+		x*) ;;
 	esac
-   fi
+	miss_str=""
+	if [ "X$RPMREBUILD_COMMENT_MISSING" = "Xyes" ]; then
+		if [ -e "$file" ]; then
+			miss_str=""
+		else 
+			miss_str='# MISSING: '
+		fi
+	fi
 
-   echo "${miss_str}${lang_str}${dir_str}${fflags_str}${attr_str}${verify_str}\"${file}\""
+	# language handling
+	[ "X$file_lang" = "X(none)" ] && file_lang=""
+	lang_str=""
+	[ "X$file_lang" = "X" ] || lang_str="%lang($file_lang) "
+   
+	# %dir handling
+	dir_str=""
+	[ "X$file_type" = "Xd" ] && dir_str="%dir "
+
+	# %fflags handling
+	if [ "X$file_flags" = "X" ]; then
+		fflags_str=""
+	else
+		for flag in $FFLAGS; do
+			if [ "X${file_flags##*${flag}*}" = "X" ]; then
+				eval ${flag}_str="\$${flag}_val"
+			else
+				eval ${flag}_str=""
+			fi
+		done
+
+		# Reset strings' values
+		config_par=""; config_full=""
+
+		config_par="${m_str}${n_str}"
+		# Handle a rpm's bug described by Han Holl:
+		# There are missignok or/and noreplace flag but no config flag
+		# In this case I simple force using '%config' string
+		[ "X$config_par" = "X" ] || c_str=$c_val
+
+		# Concatenate c_str with config_param. 
+		# If config param non-empty strip it's last character
+		config_full="${c_str}${config_par:+(${config_par%?})}" 
+
+		# If config_full string non-empty add space
+		config_full="${config_full:+${config_full} }"
+
+		fflags_str="${d_str}${config_full}${g_str}"
+	fi
+
+	# %attr handling
+	if [ "X$RPMREBUILD_PUG_FROM_FS" = "Xyes" ]; then
+		attr_str=""
+	else
+		file_perm="${file_perm#??}"
+		attr_str="%attr($file_perm $file_user $file_group) "
+	fi
+
+	# Verify handling
+	verify_str=""
+	verify_par=""
+	non_verify_par=""
+	Bit=1
+	file_verify="0$file_verify" # make it octal for shell
+	for verify_flag in $VERIFY_FLAGS; do
+		if [ $[$file_verify & $Bit] -eq 0 ]; then
+			non_verify_par="$non_verify_par$verify_flag "
+		else
+			verify_par="$verify_par$verify_flag "
+		fi
+		Bit=$[ $Bit << 1 ]
+	done
+
+	# if bit after last verify bit is off I assume that 
+	# %verify( ...) was used
+	# otherwise I assume %verify(not ...) was used. 
+	if [ $[$file_verify & $Bit] -eq 0 ]; then ## Use "verify_par"
+		# If verify_par not empty, set verify_str to %verify($verify_par)
+		# Strip last character from verify_par
+		verify_str="${verify_par:+%verify(${verify_par%?}) }" 
+	else # Use "non_verify_par
+		# If non_verify_par not empty, set verify_str to %verify(not $verify_par)
+		# Strip last character from non_verify_par
+		verify_str="${non_verify_par:+%verify(not ${non_verify_par%?}) }" 
+	fi
+
+	# test for jokers in file : globing seems not to work
+	# for performance reason, just if warning flag
+	if [ "X$RPMREBUILD_WARNING" = "Xyes" ]; then
+		case "$file" in
+			*[*?]*)
+				cat <<-WARN_TXT 1>&2 || exit
+				
+				-------------------------------- WARNING ------------------------------------------
+				file named $file contains globbing characters
+				rpm building may not work
+				-----------------------------------------------------------------------------------
+				WARN_TXT
+			;;
+		esac
+	fi
+
+	echo "${miss_str}${lang_str}${dir_str}${fflags_str}${attr_str}${verify_str}\"${file}\""
 done || exit
 exit 0
