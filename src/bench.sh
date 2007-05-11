14d8f5bdda455c081923c8ce2eef29a96adef6a1
gerbier <gerbier@f0fe687d-0691-4e6f-908c-fb32f6a794c4>
2007-05-11 11:07:07 +0000

    now clean builded package after each build to avoid file system full suppress temporary output file analyse building output to find real problems

--- src/bench.sh
+++ src/bench.sh
@@ -1,29 +1,62 @@
 #!/bin/sh
 # a script test rpmrebuild on all installed packages
 # internal use for developpers
+# $Id:$
+
+tmpdir=/tmp
+
+# to have standardize error messages
+export LC_ALL=POSIX
 
 list=$(rpm -qa | sort)
 
 total=0
+notok=0
 bad=0
 
 for pac in $list
 do
+	echo -n "$pac : "
 	let total="$total + 1"
-	rpmrebuild -b -k  -y no -c yes -d /tmp $pac > output  2>&1
+	output=$( nice rpmrebuild -b -k  -y no -c yes -d $tmpdir $pac  2>&1 )
 	irep=$?
 	if [ $irep -eq 0 ]
 	then
-		echo "$pac : ok"
+		echo "ok"
 	else
-		echo "$pac : KO"
-		cat output
-		let bad="$bad + 1"
+
+		# todo : parse output to remove false problems
+		# dependencies problem ...
+		# and only keep real rpmrebuild errors
+
+		depend=$( echo "$output" | grep "Failed dependencies" )
+		changelog=$( echo "$output" | grep "changelog not in descending chronological order" )
+		if [ -n "$depend" ]
+		then
+			echo "notok (Failed dependencies)"
+			echo "$output"
+			let notok="$notok + 1"
+
+		elif [ -n "$changelog" ]
+		then
+			echo "notok (changelog not in descending chronological order)"
+			echo "$output"
+			let notok="$notok + 1"
+		else
+			echo "KO"
+			echo "$output"
+			let bad="$bad + 1"
+		fi
 	fi
 	rm -f ${pac}.spec
-	rm -rf i386 i586 i686 noarch 2> /dev/null
+	# remove new rpm files
+	rm -f ${tmpdir}/i386/* ${tmpdir}/i586/* ${tmpdir}/i686/* ${tmpdir}/noarch/* 2> /dev/null
 done
+
+# clean temporary directories
+rmdir ${tmpdir}/i386 ${tmpdir}/i586 ${tmpdir}/i686 ${tmpdir}/noarch
 
 echo "-------------------------------------------------------"
 echo "$bad bad build on $total packages"
+echo "$notok failed build"
 echo "-------------------------------------------------------"
