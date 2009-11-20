58b9f2e04995ad9510215f91d697a4acb08e6b28
gerbier <gerbier@f0fe687d-0691-4e6f-908c-fb32f6a794c4>
2009-11-20 09:27:36 +0000

    now can translate to any type

--- src/plugins/man/conv.sh
+++ src/plugins/man/conv.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
-# conversion des fichier man d'iso-latin en utf8
+# conversion des fichier man francais entre iso-latin et utf8
+# $Id$
 
 fic=$1
 
-src="fr_FR/$fic"
+dest=$2
 
-if [ -f $src ]
+case $dest in
+	fr_FR)
+		src=fr_FR.UTF-8;
+		type=ISO_8859-15;
+		;;
+	fr_FR.UTF-8)
+		src=fr_FR;
+		type=UTF-8;
+		;;
+	*)
+		echo "syntaxe $0 fichier cible";
+		exit;
+		;;
+esac
+
+fsrc="$src/$fic"
+if [ -f $fsrc ]
 then
-	dest="fr_FR.UTF-8/$fic"
-	iconv -t UTF-8 -o $dest $src
-	diff $src $dest
+	fdest="$dest/$fic"
+	iconv -t $type -o $fdest $fsrc
+	diff $fsrc $fdest
 fi
