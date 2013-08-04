615f217896127e833b24fa161f19e608feb7bb93
gerbier <gerbier@f0fe687d-0691-4e6f-908c-fb32f6a794c4>
2013-08-04 15:47:38 +0000

    bugfix for ubuntu/debian

--- rpmtags/list_tags.sh
+++ rpmtags/list_tags.sh
@@ -3,7 +3,8 @@
 distrib=$( lsb_release -i | awk '{print $3}' )
 release=$( lsb_release -r | awk '{print $2}' )
 
-rpmver=$(rpm -q --queryformat '%{VERSION}' rpm )
+#rpmver=$(rpm -q --queryformat '%{VERSION}' rpm )
+rpmver=$(rpm --version | awk '{ print $3 }' )
 
 rpm --querytags > querytags.${distrib}${release}_rpm${rpmver}
 
