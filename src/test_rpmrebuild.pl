9314c54fbb7f28da592d55cbb3fabeb40b8d9220
Eric Gerbier <eric.gerbier@meteo.fr>
2020-12-28 13:30:03 +0100

    - force lang in english - add test for an installes rpm - add test for an rpm file

--- src/test_rpmrebuild.pl
+++ src/test_rpmrebuild.pl
@@ -23,15 +23,21 @@ use warnings;
 use Test::More qw(no_plan);
 use Data::Dumper;
 
+## no critic (ProhibitBacktickOperators)
+
 # arguments test
-my $cmd = './rpmrebuild';
+$ENV{'RPMREBUILD_ROOT_DIR'} = '.';     # force use dev code
+$ENV{'LANG'}                = 'en';    #
+my $cmd = './rpmrebuild -b';
 
 # 1 :  no arguments
 my $out = `$cmd 2>&1`;
-like( $out, qr/il manque le nom du package/, 'no parameter' ) or diag("out=$out\n");
+like( $out, qr/package argument missing/, 'no parameter' )
+  or diag("out=$out\n");
 
 # 2 version
 $out = `$cmd --version 2>&1`;
+## no critic (ProhibitEscapedMetacharacters)
 like( $out, qr/\d+\./, 'version' ) or diag("out=$out\n");
 
 # 3 help
@@ -40,19 +46,28 @@ like( $out, qr/options:/, 'help' ) or diag("out=$out\n");
 
 # 4 bad package
 $out = `$cmd tototo 2>&1`;
-like( $out, qr/n'est pas installé/, 'no package' ) or diag("out=$out\n");
+like( $out, qr/is not installed/, 'no package' ) or diag("out=$out\n");
 
 # 5 multiple package
 $out = `$cmd gpg-pubkey 2>&1`;
-like( $out, qr/plusieurs packages correspondent/, 'multiple package' ) or diag("out=$out\n");
+like( $out, qr/too many packages match/, 'multiple package' )
+  or diag("out=$out\n");
 
 # 6 bad file name
 $out = `$cmd -p tototo 2>&1`;
-like( $out, qr/Fichier non trouvé/, 'no file' ) or diag("out=$out\n");
+like( $out, qr/File not found/, 'no file' ) or diag("out=$out\n");
 
 # 7 file not from package
 $out = `$cmd -p Todo 2>&1`;
-like( $out, qr/n'est pas un fichier rpm/, 'not an rpm file' ) or diag("out=$out\n");
+like( $out, qr/is not an rpm file/, 'not an rpm file' ) or diag("out=$out\n");
+
+# 8 package
+$out = `$cmd afick-doc 2>&1`;
+like( $out, qr/result:.*afick-doc/, 'rpm package' ) or diag("out=$out\n");
+
+# 9 file
+$out = `$cmd -p ../test/afick-doc-3.7.0-1.noarch.rpm 2>&1`;
+like( $out, qr/result:.*afick-doc/, 'rpm file' ) or diag("out=$out\n");
 
 # capabilities
 # rpmrebuild iputils
