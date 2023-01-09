3c3b648a3a488aa7a2566d48ee164c37b9acd200
Eric Gerbier <gerbier@users.sourceforge.net>
2023-01-09 14:56:51 +0100

    rename additional env into RPMREBUILD_additional

--- src/rpmrebuild.sh
+++ src/rpmrebuild.sh
@@ -226,7 +226,7 @@ function RpmBuild
 			return 1
 		}
 	fi
-	eval "$change_arch" $BUILDCMD --define "'buildroot $BUILDROOT'" "$rpm_defines" -bb "$rpm_verbose" "$additional" "${FIC_SPEC}" || {
+	eval "$change_arch" $BUILDCMD --define "'buildroot $BUILDROOT'" "$rpm_defines" -bb "$rpm_verbose" "$RPMREBUILD_additional" "${FIC_SPEC}" || {
 		Error "(RpmBuild) package '${PAQUET}' $BuildFailed"
 		return 1
 	}
